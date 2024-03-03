31ccfbf86d0dae4f3225add26cb701bf00efe826
sho-pc123 <syouteni123@gmail.com>
2024-03-03 15:45:05 +0900

    型推論のよる省略

--- src/callback.ts
+++ src/callback.ts
@@ -15,12 +15,12 @@ const users: User[] = [
 ]
 
 // 名前だけを抽出した配列
-const names: string[] = users.map((u: User): string => u.name);
+const names: string[] = users.map((u: User) => u.name);
 // 20歳以上のユーザーだけの配列
-const adultUsers: User[] = users.filter((user: User): boolean => user.age >= 20);
+const adultUsers = users.filter((user: User) => user.age >= 20);
 // すべてのユーザーが20歳以上ならtrue
-const allAdult: boolean = users.every((user: User):boolean => user.age >= 20);
+const allAdult = users.every((user: User) => user.age >= 20);
 // 60歳以上のユーザーが1任でもいればtrue
-const seniourExist: boolean = users.some((user: User): boolean => user.age >= 60);
+const seniourExist = users.some((user: User) => user.age >= 60);
 // 名前がsatouで始まるユーザーを探して返す
-const satou: User | undefined = users.find((user: User): boolean => user.name.startsWith("satou"));
+const satou = users.find((user: User) => user.name.startsWith("satou"));
