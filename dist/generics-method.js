cbed24fa7da61c25739580990aea16edfd05eeb1
sho-pc123 <syouteni123@gmail.com>
2024-03-03 22:27:14 +0900

    ジェネリクス関数 - メソッド記法

---
+++ dist/generics-method.js
@@ -0,0 +1,10 @@
+"use strict";
+const utils = {
+    reoeat(element, length) {
+        const result = [];
+        for (let i = 0; i < length; i++) {
+            result.push(element);
+        }
+        return result;
+    }
+};
