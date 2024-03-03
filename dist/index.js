290ca35e0e4ecfcf11aeee7ba5144ce942c86ca3
sho-pc123 <syouteni123@gmail.com>
2024-03-03 14:14:17 +0900

    restパラメーター機能

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,10 @@
 "use strict";
-const message = "Hello,world!";
-console.log(message);
+const sum = (base, ...args) => {
+    let result = base * 100;
+    for (const num of args) {
+        result += 100;
+    }
+    return result;
+};
+console.log(sum(1, 12, 100));
+console.log(sum(234));
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
-const message: string = "Helo,world!";
+const sum = (base: number, ...args: number[]): number => {
+  let result = base * 100;
+  for(const num of args){
+    result += 100;
+  }
+  return result;
+}
 
-console.log(message)
+console.log(sum(1,12,100));
+// ...argsの「Rest Parameters」機能により、関数は任意の数の引数を受け取ることができ、引数が1つでも、複数でも問題ない
+console.log(sum(234));
