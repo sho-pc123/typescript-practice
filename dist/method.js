0cdf56f4528c1d69b00797dbdd31c3bd2fb8f3ec
sho-pc123 <syouteni123@gmail.com>
2024-03-03 22:24:28 +0900

    メソッド記法

---
+++ dist/method.js
@@ -0,0 +1,11 @@
+"use strict";
+const obj = {
+    // メソッド記法
+    double(num) {
+        return num * 2;
+    },
+    // 通常の記法 + アロー関数
+    double2: (num) => {
+        return num * 2;
+    }
+};
---
+++ src/function/method.ts
@@ -0,0 +1,12 @@
+const obj = {
+  // メソッド記法
+  double(num: number): number {
+    return num * 2;
+  },
+
+  // 通常の記法 + アロー関数
+  double2: (num: number): number => {
+    return num * 2;
+  }
+}
+
