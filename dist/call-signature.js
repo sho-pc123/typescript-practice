365dc0e38040daf60e0cbfa8548138bcb628041c
sho-pc123 <syouteni123@gmail.com>
2024-03-03 15:57:24 +0900

    型推論のよる省略

---
+++ dist/call-signature.js
@@ -0,0 +1,9 @@
+"use strict";
+const double = (arg) => {
+    console.log(arg * 2);
+};
+//  doubleはisUsedプロパティを持つ
+double.isUsed = true;
+console.log(double.isUsed);
+// doubleは関数として呼び出せる
+double(100);
