8b481e954af261954568292c1b3cdb8a9a56083c
sho-pc123 <syouteni123@gmail.com>
2024-03-06 08:49:27 +0900

    ジェネリクス

--- dist/function/generics.js
+++ dist/function/generics.js
@@ -1,6 +1,6 @@
 "use strict";
 // 型引数を持つ関数（ジェネリック関数）
-function repeat(element, length) {
+function repeat3(element, length) {
     const result = [];
     for (let i = 0; i < length; i++) {
         result.push(element);
