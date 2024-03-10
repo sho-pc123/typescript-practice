484acae07e260c5d5361eb592d1244cd7bb09566
sho-pc123 <syouteni123@gmail.com>
2024-03-11 08:43:57 +0900

    一括import

---
+++ src/module/bundle-import.ts
@@ -0,0 +1,10 @@
+export const name = "uhyo";
+export const age = 26;
+
+import * as uhyo from "./export";
+
+// importした値をuhyoという変数に格納するため.記法でアクセスすることが可能になる
+// "uhyoと表示される"
+console.log(uhyo.name);
+// "26"と表示される
+console.log(uhyo.age);
